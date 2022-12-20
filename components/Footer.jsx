c202d9758c7a3be4e0bdf65c2d8b67760c7dc2e5
shantanu sahay <71556064+shan0720-creator@users.noreply.github.com>
2022-12-20 15:48:58 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+  return <footer>
+       <div>
+       <h1>TechSoft</h1>
+       <p>@All right reserved</p>
+       </div>
+       <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href ="https://www.youtube.com/watch?v=MAiVqgN1WhY">Youtube</a>
+            <a href ="https://www.instagram.com/k9_asmodeus/?hl=en">Instagram</a>
+        </div>
+       </div>
+      </footer>
+}
+
+export default Footer
